79f6c1bb784b490ac6c0bd07b9153878835d619d
Shivansh <shivanshpahwa24@gmail.com>
2021-03-31 05:24:42 +0530

    Some bugs remaining

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -8,9 +8,10 @@ let socket;
 const Chat = ({ location }) => {
   const [name, setName] = useState("");
   const [room, setRoom] = useState("");
+  const [msg, setMsg] = useState("");
+  const [users, setUsers] = useState([]);
   const ENDPOINT = "localhost:5000";
-
-  const chatMessages = document.querySelector(".chat-messages");
+  const [messages, setMessages] = useState([]);
 
   useEffect(() => {
     const { name, room } = queryString.parse(location.search);
@@ -18,63 +19,64 @@ const Chat = ({ location }) => {
 
     setName(name);
     setRoom(room);
-    console.log(name, room);
 
     // Join chatroom
     socket.emit("joinRoom", { name, room });
 
     // Get room and users
     socket.on("roomUsers", ({ room, users }) => {
-      outputRoomName(room);
-      outputUsers(users);
+      setUsers(users);
     });
 
     // Message from server
     socket.on("message", (message) => {
-      outputMessage(message);
-
-      chatMessages.scrollTop = chatMessages.scrollHeight;
-    });
-
-    // Message submit
-    document.getElementById("chat-form").addEventListener("submit", (e) => {
-      e.preventDefault();
-
-      const msg = e.target.elements.msg.value;
-
-      socket.emit("chatMessage", msg);
-
-      e.target.elements.msg.value = "";
-      e.target.elements.msg.focus();
+      setMessages([...messages, message]);
     });
   }, [ENDPOINT, location.search]);
 
   // Output message to DOM
-  const outputMessage = (message) => {
-    const div = document.createElement("div");
-    div.classList.add("message");
-    div.innerHTML = `	<p class="meta">${message.username} <span>${message.time}</span></p>
-  <p class="text">
-    ${message.text}
-  </p>`;
-
-    chatMessages.appendChild(div);
-  };
-
-  // Add room name to DOM
-  const outputRoomName = (room) => {
-    document.getElementById("room-name1").innerText = room;
-    document.getElementById("room-name2").innerText = room;
+  const OutputMessage = (messages) => {
+    /*  document.querySelector(".chat-messages").scrollTop = document.querySelector(
+      ".chat-messages"
+    ).scrollHeight; */
+    console.log(messages);
+    return <div></div>;
+    /* return (
+      <>
+        {messages.map((message) => (
+          <div className="message">
+            <p className="meta">
+              {message.username} <span>{message.time}</span>
+            </p>
+            <p className="text">{message.text}</p>
+          </div>
+        ))}
+      </>
+    ); */
   };
 
   //Add users to DOM
-  const outputUsers = (users) => {
-    document.getElementById("users1").innerHTML = `${users
-      .map((user) => `<li>${user.username}</li>`)
-      .join("")}`;
-    document.getElementById("users2").innerHTML = `${users
-      .map((user) => `<li>${user.username}</li>`)
-      .join("")}`;
+  const OutputUsers = (users) => {
+    console.log(users);
+    return <div></div>;
+    /* return (
+      <>
+        {users.map((user) => (
+          <li>{user.username}</li>
+        ))}
+      </>
+    ); */
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    socket.emit("chatMessage", msg);
+
+    setMsg("");
+    OutputUsers(users);
+    OutputMessage(messages);
+    document.getElementById("msg").focus();
   };
 
   return (
@@ -93,13 +95,13 @@ const Chat = ({ location }) => {
           <h6>
             <i className="fas fa-comments"></i> Room Name
           </h6>
-          <h5 id="room-name1"></h5>
+          <h5>{room}</h5>
         </div>
         <div>
           <h6>
             <i className="fas fa-users"></i> Users
           </h6>
-          <ul id="users1"></ul>
+          <ul id="users1">{/* <OutputUsers users={users} /> */}</ul>
         </div>
       </div>
       <main className="chat-main">
@@ -107,24 +109,29 @@ const Chat = ({ location }) => {
           <h4>
             <i className="fas fa-comments"></i> Room Name
           </h4>
-          <h2 id="room-name2"></h2>
+          <h2>{room}</h2>
           <h4>
             <i className="fas fa-users"></i> Users
           </h4>
-          <ul id="users2"></ul>
+          <ul id="users2">{/* {users && <OutputUsers users={users} />} */}</ul>
         </div>
         <div className="chat-box">
-          <div className="chat-messages"></div>
+          <div className="chat-messages">
+            {/* <OutputMessage messages={messages} /> */}
+          </div>
           <div className="chat-form-container shadow-lg">
-            <form id="chat-form">
+            <form id="chat-form" onSubmit={handleSubmit}>
               <input
                 id="msg"
                 type="text"
                 placeholder="Enter Message"
                 required
+                value={msg}
+                onChange={(e) => setMsg(e.target.value)}
+                name="msg"
                 autoComplete="off"
               />
-              <button className="chat-form-btn">
+              <button className="chat-form-btn" type="submit">
                 <i className="fas fa-paper-plane"></i>
               </button>
             </form>
